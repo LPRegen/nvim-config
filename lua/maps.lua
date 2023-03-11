a89d487867501c01359e8ea3f405dd339822baf5
LPRegen <manuel.escribano.051@gmail.com>
2023-03-11 23:21:15 +0000

    feat: add mappings

--- lua/maps.lua
+++ lua/maps.lua
@@ -16,8 +16,8 @@ keymap.set('n', '<C-a>', 'gg<S-v>G')
 -- New tab
 keymap.set('n', 'te', ':tabedit<Return>', { silent = true })
 -- Split window
-keymap.set('n', 'ss', ':split<Return><C-w>w', { silent = true})
-keymap.set('n', 'sv', ':vsplit<Return><C-w>w', { silent = true})
+keymap.set('n', 'ss', ':split<Return><C-w>w', { silent = true })
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w', { silent = true })
 -- Move window
 keymap.set('n', '<Space>', '<C-w>w')
 keymap.set('', 's<left>', '<C-w>h')
@@ -35,3 +35,5 @@ keymap.set('n', '<C-w><right>', '<C-w>>')
 keymap.set('n', '<C-w><up>', '<C-w>+')
 keymap.set('n', '<C-w><down>', '<C-w>-')
 
+-- Save
+keymap.set('n', '<C-s>', ':w<CR>')
