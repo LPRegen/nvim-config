3e250eeff0d03f4f3b601de5c091fe6b1f7e52d9
LPRegen <manuel.escribano.051@gmail.com>
2023-05-01 10:36:21 +0200

    feat: change package manager to Lazy

--- README.md
+++
@@ -1,2 +0,0 @@
-# nvim-config
-Personal Nvim configuration
---
+++ lua/config/lazy.lua
@@ -0,0 +1,35 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+
+require("lazy").setup({
+	spec = {
+		{ import = "plugins" },
+		{ import = "plugins.extras.lang.typescript" },
+	},
+	defaults = {
+		lazy = true,
+		version = false,
+	},
+	install = {
+		colorscheme = { "catppuccin" },
+	},
+	checker = {
+		enabled = true,
+	},
+})
+
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
