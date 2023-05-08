3a1f422cbccb1216588676be74b233e8855b0a52
LPRegen <manuel.escribano.051@gmail.com>
2023-05-08 18:48:01 +0200

    feat: add nvim surround

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -208,4 +208,17 @@ return {
 			},
 		},
 	},
+
+	-- Nvim surrounding
+
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
 }
