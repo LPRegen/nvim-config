14641dc50fd5cc2e0fe939317882033051ae8ff0
LPRegen <manuel.escribano.051@gmail.com>
2023-05-26 08:24:22 +0200

    refactor: neo tree - change icons - add keybinding

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -17,12 +17,33 @@ return {
 					["l"] = "open",
 				},
 			},
+			default_component_configs = {
+				indent = {
+					with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+					expander_collapsed = "",
+					expander_expanded = "",
+					expander_highlight = "NeoTreeExpander",
+				},
+				git_status = {
+					symbols = {
+						modified = "",
+						deleted = " ",
+						unstaged = " ",
+						untracked = " ",
+					},
+				},
+			},
 		},
 		keys = {
 			{
 				"<leader>e",
 				"<cmd>Neotree toggle<CR>",
 				desc = "Open Neotree",
+			},
+			{
+				"<leader>gn",
+				"<cmd>Neotree float git_status<CR>",
+				desc = "Neotree status",
 			},
 		},
 	},
