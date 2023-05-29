26e86d85bcc736aca23ba69143e4003beb281278
LPRegen <manuel.escribano.051@gmail.com>
2023-05-29 17:36:12 +0200

    refactor: remove theme transparency

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,7 +7,7 @@ return {
 		config = function()
 			require("tokyonight").setup({
 				style = "night",
-				transparent = true,
+				-- transparent = true,
 			})
 			vim.cmd([[colorscheme tokyonight]])
 		end,
