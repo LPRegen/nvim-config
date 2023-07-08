af2638e44c85f7c1bf8009da3cec738013bad5b4
LPRegen <manuel.escribano.051@gmail.com>
2023-07-08 14:21:09 +0200

    refactor: add nvim-ts-autotag

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      autotag = {
+        enable = true,
+      },
+      dependencies = {
+        {
+          "windwp/nvim-ts-autotag",
+        },
+      },
+    },
+  },
+
+  -- Auto tag
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+}
