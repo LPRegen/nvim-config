6e476b861b047990d41602bc41d65c4d3aaef828
LPRegen <manuel.escribano.051@gmail.com>
2023-07-15 02:41:53 +0200

    refactor: remove unused variable

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -4,7 +4,6 @@ return {
     opts = function()
       vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
       local cmp = require("cmp")
-      local luasnip = require("luasnip")
       local defaults = require("cmp.config.default")()
       return {
         completion = {
