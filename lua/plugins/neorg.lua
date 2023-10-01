0da53a9f28d5e9f8903bfe20f3c71947517eb127
LPRegen <manuel.escribano.051@gmail.com>
2023-10-01 23:40:41 +0200

    refactor: add neorg

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,55 @@
+return {
+  -- https://github.com/nvim-neorg/neorg/wiki
+  "nvim-neorg/neorg",
+  build = ":Neorg sync-parsers",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  cmd = "Neorg",
+  config = function()
+    require("neorg").setup({
+      load = {
+        ["core.defaults"] = {}, -- Loads default modules
+        ["core.concealer"] = {
+          config = {
+            folds = false,
+            icon_preset = "diamond",
+            icons = {
+              todo = {
+                uncertain = {
+                  icon = "",
+                },
+                urgent = {
+                  icon = "",
+                },
+              },
+            },
+          },
+        }, -- Adds pretty icons to your documents
+        ["core.completion"] = {
+          config = {
+            engine = "nvim-cmp",
+          },
+        },
+        ["core.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              notes = "~/dev/personalNotes/Neorg",
+            },
+            default_workspace = "notes",
+          },
+        },
+        ["core.integrations.treesitter"] = {},
+        ["core.export"] = {
+          config = {
+            export_dir = "~/dev/mdExported",
+          },
+        },
+      },
+    })
+  end,
+  keys = {
+    { "<leader>ni", "<cmd>Neorg index<cr>", desc = "Index" },
+    { "<leader>nt", "<cmd>Neorg journal today<cr>", desc = "Today's journal" },
+    { "<leader>nT", "<cmd>Neorg journal tomorrow<cr>", desc = "Tomorrow's journal" },
+    { "<leader>ny", "<cmd>Neorg journal yesterday<cr>", desc = "Yesterday's journal" },
+  },
+}
