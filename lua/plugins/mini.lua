815492ec48af92635f0a697c7e2231c16494af29
LPRegen <manuel.escribano.051@gmail.com>
2023-10-03 05:07:49 +0200

    refactor: modify mapping for comments

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,14 +1,26 @@
 return {
-  "echasnovski/mini.surround",
-  opts = {
-    mappings = {
-      add = "gsa",
-      delete = "gsd",
-      find = "gsf",
-      find_left = "gsF",
-      highlight = "gsh",
-      replace = "gsr",
-      update_n_lines = "gsn",
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "gsa",
+        delete = "gsd",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        replace = "gsr",
+        update_n_lines = "gsn",
+      },
+    },
+  },
+
+  {
+    "echasnovski/mini.comment",
+    opts = {
+      mappings = {
+        comment = "<leader>/",
+        comment_line = "<leader>/",
+      },
     },
   },
 }
