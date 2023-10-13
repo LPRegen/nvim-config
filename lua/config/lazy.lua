5d3975a5339715800004b3de1aee1dd8205cfdfe
LPRegen <manuel.escribano.051@gmail.com>
2023-10-13 15:14:22 +0200

    style: modify import order

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,10 +12,12 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
 
     { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+
     { import = "lazyvim.plugins.extras.lang.docker" },
+
     { import = "lazyvim.plugins.extras.lang.python" },
 
     -- import/override with your plugins
