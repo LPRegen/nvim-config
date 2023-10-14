3378c8a6fa6f477235c0978a2676e9ab61fc343b
LPRegen <manuel.escribano.051@gmail.com>
2023-10-14 22:19:25 +0200

    refactor(treesitter): modify autotag opts

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,9 @@ return {
     opts = {
       autotag = {
         enable = true,
+        enable_rename = true,
+        enable_close = true,
+        enable_close_on_slash = true,
       },
       dependencies = {
         {
