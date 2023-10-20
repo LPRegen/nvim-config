9d9e345ee6c465a0e74e0fe349c31e1a745bf9ce
LPRegen <manuel.escribano.051@gmail.com>
2023-10-20 18:01:05 +0200

    refactor(conform|none): replace none-ls with conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    -- NOTE: https://github.com/stevearc/conform.nvim
+    "stevearc/conform.nvim",
+    ---@class ConformOpts
+    opts = {
+      formatters_by_ft = {
+        ["lua"] = { "stylua" },
+        ["javascript"] = { { "prettierd", "prettier" } },
+        ["javascriptreact"] = { { "prettierd", "prettier" } },
+        ["typescript"] = { { "prettierd", "prettier" } },
+        ["typescriptreact"] = { { "prettierd", "prettier" } },
+        ["vue"] = { { "prettierd", "prettier" } },
+        ["css"] = { { "prettierd", "prettier" } },
+        ["scss"] = { { "prettierd", "prettier" } },
+        ["less"] = { { "prettierd", "prettier" } },
+        ["html"] = { { "prettierd", "prettier" } },
+        ["json"] = { { "prettierd", "prettier" } },
+        ["jsonc"] = { { "prettierd", "prettier" } },
+        ["yaml"] = { { "prettierd", "prettier" } },
+        ["markdown"] = { { "prettierd", "prettier" } },
+        ["markdown.mdx"] = { { "prettierd", "prettier" } },
+        ["graphql"] = { { "prettierd", "prettier" } },
+        ["handlebars"] = { { "prettierd", "prettier" } },
+        tailwindcss = { "rustywind" },
+        python = { "black" },
+        htmldjango = { "djlint" },
+      },
+    },
+  },
+}
