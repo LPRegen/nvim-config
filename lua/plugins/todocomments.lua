b2fa0be516d1b86b2755b6bfa036d55185fee283
LPRegen <manuel.escribano.051@gmail.com>
2023-10-27 02:51:00 +0200

    refactor(todo-comm): extend keywords

---
+++ lua/plugins/todocomments.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "folke/todo-comments.nvim",
+    opts = {
+      keywords = {
+        NTH = { icon = " ", color = "test" },
+        TEST = { icon = "󰙨", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+      },
+    },
+  },
+}
